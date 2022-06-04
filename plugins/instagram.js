19990b62d57d9d33247c40ca78dc649fe514316d
anzilgamingyt1 <88873868+anzilgamingyt1@users.noreply.github.com>
2022-06-04 23:11:07 +0530

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,10 +1,10 @@
-/* Copyright (C) 2022 Sourav KL11.
+/* Copyright (C) 2022 dlxluttapi.
 
 Licensed under the  GPL-3.0 License;
 
 you may not use this file except in compliance with the License.
 
-Raganork MD - Sourav KL11
+ lbfc - dlxluttapi
 
 */
 
